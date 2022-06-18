bc3fce986f8a38ad231825775d2eacb50ec06911
Nhat Le <mnhat3896@gmail.com>
2022-06-18 14:47:48 +0700

    Refactor code, move vm to module (#3)
    ok

--- VirtualMachine/modules/compute/data.tf
+++ VirtualMachine/modules/compute/data.tf
@@ -1,6 +1,6 @@
 # Render a part using a `template_file`
 data "template_file" "script" {
-  template = "${file("${path.module}/scripts/init.tpl")}"
+  template = file("${path.module}/scripts/init.tpl")
 
   vars = {
     # Put argument to pass to init.tpl in here
@@ -15,6 +15,6 @@ data "template_cloudinit_config" "config" {
   part {
     filename     = "init.cfg"
     content_type = "text/cloud-config"
-    content      = "${data.template_file.script.rendered}"
+    content      = data.template_file.script.rendered
   }
 }
